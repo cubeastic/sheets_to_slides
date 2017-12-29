0f2235679e1f364c6fe4434f77b8c3f47fc34b22
Dennis Zheleznyak <>
2017-12-29 13:01:35 +0200

    functions descritipions

--- sheets_to_slides.py
+++ sheets_to_slides.py
@@ -8,6 +8,7 @@ from sys import exit    # Import the exit function
 from os import path     # Import all the os functions
 
 
+# A class to import text from Google Sheets and export it to Slides with pictures as backgrounds
 class SheetsToSlides:
 
     def __init__(self):
@@ -23,15 +24,18 @@ class SheetsToSlides:
         self.full_addr = ""
         self.jdata = ""
 
+    # Loop through the config file and return the requested value from the requested tag
     def get_config(self, field):
         t = Et.parse(self.config_file)
         for i in t.getroot():
             if i.tag == field:
                 return i.text
 
+    # Split the address given to multiple parts using a / as separator and return the sheet ID
     def extract_id(self):
         return self.sheet_addr.split("/")[5]
 
+    # The sheet must be published, otherwise it cannot be accessed
     def published_check(self):
         try:
             if requests.get(self.json_format.format(self.extract_id())).status_code == 200:
@@ -41,6 +45,7 @@ class SheetsToSlides:
         except ValueError:
             return False
 
+    # Count the rows that has data and use it as a counter to loop through and collect the quotes
     def get_quotes(self):
         rows = int(self.jdata["feed"]["openSearch$totalResults"]["$t"])
         quotes = []
@@ -48,6 +53,7 @@ class SheetsToSlides:
             quotes.append(str(self.jdata["feed"]["entry"][quote]["content"]["$t"]))
         return quotes
 
+    # Pull the data from the first column, from all the rows
     def sheets_phase(self):
         if self.published_check():
             return self.get_quotes()
